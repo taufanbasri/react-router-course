0c6f7efee5062e621313b83c9b0bb35e22fe032c
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-10-20 13:18:02 +0700

    Navbar Links

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import { Route, Routes } from "react-router-dom";
 import About from "./components/About";
 import Home from "./components/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />}></Route>
-      <Route path="about" element={<About />}></Route>
-    </Routes>
+    <>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="about" element={<About />}></Route>
+      </Routes>
+    </>
   );
 }
 
