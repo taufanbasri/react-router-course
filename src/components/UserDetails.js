5257ea928098b11715ff8d118279866a92195e50
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2022-10-20 17:21:33 +0700

    URL Params

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,8 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom'
 
 const UserDetails = () => {
+
+  const { userId } = useParams()
+
   return (
-    <div>Details about user</div>
+    <div>Details about user {userId}</div>
   )
 }
 
--- src/components/Users.js
+++ src/components/Users.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { Outlet } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 const Users = () => {
   return (
     <div>
-      <h2>User 1</h2>
-      <h2>User 2</h2>
-      <h2>User 3</h2>
+      <h2>
+        <Link to={'/users/1'}>User 1</Link>
+      </h2>
+      <h2>
+        <Link to={'/users/2'}>User 2</Link>
+      </h2>
+      <h2>
+        <Link to={'/users/3'}>User 3</Link>
+      </h2>
 
       <Outlet />
     </div>
